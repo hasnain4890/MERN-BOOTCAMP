7a6629d31adf19255d6b3fcbb7c525e5cbe3506a
hasnain ahmad <hasnainahmad4890@gmail.com>
2022-10-29 12:41:50 +0500

    all assignments and tasks added

---
+++ classwork/29_oct_22/forloop.js
@@ -0,0 +1,10 @@
+function printNumbers(array) {
+  let num = 1;
+  for (var i = 1; i <= 10; i++) {
+    if (i == num) {
+      //console.log(i);
+    }
+    num++;
+  }
+}
+printNumbers([1, 2, 3, 4, 5, 7]);
